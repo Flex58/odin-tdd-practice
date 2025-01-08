cc4b4bf8b7ffbf62c07fd1b773cffe66968a592a
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-01-08 11:01:32 +0100

    test: add reverse, calculator and caesarCypher

--- practice.js
+++ practice.js
@@ -1,12 +1,52 @@
 const capitalize = (string) => {
-    return "Test"
-}
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+};
 
-const test = () => {
-    return "asfnkaslokfn"
+const reverseString = (string) => {
+  const arr = string.split("");
+  return arr.reverse().join("");
+};
+
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+
+  subtract(a, b) {
+    return a - b;
+  },
+
+  multiply(a,b){
+    return a * b
+  },
+
+  divide(a,b){
+    return a / b
+  }
+
+};
+
+const caesarCypher = (string, key) => {
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const lower = "abcdefghijklmnopqrstuvwxyz"
+    const arr = upper.split("")
+    const arr2 = lower.split("")
+    let test = string.split("")
+
+    for (let char = 0; char < test.length; char ++){
+        if (upper.includes(test[char])) {
+            test[char] = upper[(upper.indexOf(test[char]) + key) % upper.length]
+        }
+        else if(lower.includes(test[char])) {
+            test[char] = lower[(lower.indexOf(test[char]) + key) % upper.length]
+        }
+    };
+    return test.join("")
 }
 
 module.exports = {
-    capitalize,
-    test
-}
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCypher,
+};
--- pratice.test.js
+++ pratice.test.js
@@ -1,5 +1,21 @@
-const {capitalize, foo} = require("./practice.js")
+const { capitalize, reverseString, calculator, caesarCypher } = require("./practice.js");
 
-test("capitalize", () =>  {
-    expect(capitalize("test")).toBe("Test")
+test("capitalize", () => {
+  expect(capitalize("test")).toBe("Test");
+});
+
+test("reverse", () => {
+  expect(reverseString("lane")).toBe("enal");
+});
+
+test("calculator", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+  expect(calculator.subtract(2, 1)).toBe(1);
+  expect(calculator.multiply(5, 2)).toBe(10);
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test("caesar cypher", () => {
+    expect(caesarCypher("xyz", 3)).toBe("abc")
+    expect(caesarCypher("HeLLo", 3)).toBe("KhOOr")
 })
